45eff2ac1a04ea74629436c500c6ae8a034a0cbe
JUNBYEONGHA <69623629+JUNBYEONGHA@users.noreply.github.com>
2022-01-15 22:18:58 +0900

    2022-01-15
    2022-01-15

---
+++ BAEKJOON/Gold/타일 채우기.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<cstring>
+
+#define endl "\n"
+using namespace std;
+
+#define MAX 35
+
+int N;
+int DP[MAX];
+
+void Input()
+{
+	cin >> N;
+}
+void Solution()
+{
+	if (N % 2 == 1)
+	{
+		cout << 0 << endl;
+		return;
+	}
+	DP[0] = 1;
+	DP[1] = 0;
+	DP[2] = 3;
+	for (int i = 4; i <= N; i = i + 2)
+	{
+		DP[i] = DP[i - 2] * DP[2];
+		for (int j = i - 4; j >= 0; j = j - 2)
+		{
+			DP[i] = DP[i] + (DP[j] * 2);
+		}
+	}
+	cout << DP[N] << endl;
+}
+void Answer()
+{
+	Input();
+    Solution();
+}
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Answer();
+	return 0;
+}
