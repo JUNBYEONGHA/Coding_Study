9c3c11d0f213b9d136514968e8821b21e4c48647
JUNBYEONGHA <69623629+JUNBYEONGHA@users.noreply.github.com>
2022-02-05 09:03:51 +0900

    2022-02-05
    2022-02-05

---
+++ BAEKJOON/Silver/어린 왕자.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+#define endl "\n"
+using namespace std;
+void Answer()
+{
+	int n, T;
+	int enter, departure;
+	int x, y, r, x1, y1, x2, y2;
+	cin >> T;
+	while (T--)
+	{
+		cin >> x1 >> y1 >> x2 >> y2;
+		cin >> n;
+		enter = 0;
+		departure = 0;
+		while (n--)
+		{
+			cin >> x >> y >> r;
+
+			if ((x - x1) * (x - x1) + (y - y1) * (y - y1) < r * r)
+				if ((x - x2) * (x - x2) + (y - y2) * (y - y2) > r * r)
+					departure++;
+
+			if ((x - x1) * (x - x1) + (y - y1) * (y - y1) > r * r)
+				if ((x - x2) * (x - x2) + (y - y2) * (y - y2) < r * r)
+					enter++;
+		}
+		cout << enter + departure << '\n';
+	}
+}
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Answer();
+	return 0;
+}
