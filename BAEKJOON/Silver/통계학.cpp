a25e38887b2f707483bc4bd075b585ca2610bd7e
JUNBYEONGHA <69623629+JUNBYEONGHA@users.noreply.github.com>
2022-02-28 10:36:30 +0900

    2022-02-28
    2022-02-28

--- BAEKJOON/Silver/통계학.cpp
+++ BAEKJOON/Silver/통계학.cpp
@@ -1,34 +1,32 @@
 #include<iostream>
-#include<queue>
-#include<string>
 #include<vector>
-#include<cmath>
 #include<algorithm>
+#include<cmath>
+#include<string>
 
 #define endl "\n"
 using namespace std;
+
 int num;
 int arr[500001];
 int DP[8001];
-void Input()
+
+void Answer()
 {
 	cin >> num;
 	for (register int i = 0; i < num; i++)
 	{
 		cin >> arr[i];
 	}
-}
-void Solution()
-{
 	sort(arr, arr + num);
 	int sum = 0;
 	for (register int i = 0; i < num; i++)
 	{
 		sum += arr[i];
-		DP[arr[i] + 4000]++;	
+		DP[arr[i] + 4000]++;
 	}
 	int maxcount = 0;
-	int index = 0 , value = 0;
+	int index = 0, value = 0;
 	for (register int i = 0; i < 8001; i++)
 	{
 		if (DP[i] > maxcount)
@@ -46,17 +44,16 @@ void Solution()
 			break;
 		}
 	}
-	cout << round((float)sum / num) << endl;
+	if (round((float)sum / num) == 0)
+		cout << 0 << endl;
+	else
+		cout << round((float)sum / num) << endl;
 	cout << arr[num >> 1] << endl;
 	cout << value << endl;
-	cout << arr[num-1] - arr[0] << endl;
-	
+	cout << arr[num - 1] - arr[0] << endl;
+
 }
-void Answer()
-{
-	Input();
-	Solution();
-}
+
 int main()
 {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
