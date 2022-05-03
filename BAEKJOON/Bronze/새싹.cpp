161a31b6638bd60bfe0ac88c08e14be20878d067
JUNBYEONGHA <69623629+JUNBYEONGHA@users.noreply.github.com>
2022-05-04 00:32:25 +0900

    2022-05-04
    2022-05-04

---
+++ BAEKJOON/Bronze/새싹.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#define endl "\n"
+using namespace std;
+
+void Answer()
+{
+	cout << "         ,r'\"7" << endl
+		"r`-_   ,'  ,/" << endl
+		" \\. \". L_r'" << endl
+		"   `~\\/" << endl
+		"      |" << endl
+		"      |";
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	Answer();
+	return 0;
+}
