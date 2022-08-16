6e2e3780ee730691aa820832a90f9dfd2ffb5883
Jun ByeongHa <69623629+JUNBYEONGHA@users.noreply.github.com>
2022-08-16 19:01:54 +0900

    2022-08-16
    2022-08-16

---
+++ BAEKJOON/Gold/가장 오래 걸리는 스도쿠.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#define endl "\n"
+using namespace std;
+
+void Answer()
+{
+	cout << "8 0 0 0 0 0 0 0 0\n"
+		"0 0 3 6 0 0 0 0 0\n"
+		"0 7 0 0 9 0 2 0 0\n"
+		"0 5 0 0 0 7 0 0 0\n"
+		"0 0 0 0 4 0 7 0 0\n"
+		"0 0 0 1 0 0 0 3 0\n"
+		"0 0 1 0 0 0 0 0 8\n"
+		"0 0 8 5 0 0 0 1 0\n"
+		"0 9 0 0 0 0 4 0 0";
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	Answer();
+	return 0;
+}
