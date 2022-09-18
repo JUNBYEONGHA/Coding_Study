6f8b4a8370c16914e6ae1563d11fa4dab4c51a3d
Jun ByeongHa <69623629+JUNBYEONGHA@users.noreply.github.com>
2022-09-18 23:38:55 +0900

    2022-09-18
    2022-09-18

---
+++ BAEKJOON/Silver/돌 게임.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#define endl "\n"
+using namespace std;
+
+void Answer()
+{
+	int N;
+	cin >> N;
+	if (N % 2 == 1)
+		cout << "SK";
+	else
+		cout << "CY";
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	Answer();
+	return 0;
+}
