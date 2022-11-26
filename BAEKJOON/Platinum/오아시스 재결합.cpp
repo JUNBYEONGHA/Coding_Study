62afd7a6dbe9d81767e545dd775a407236c244c8
Jun ByeongHa <69623629+JUNBYEONGHA@users.noreply.github.com>
2022-11-26 13:33:30 +0900

    2022-11-26
    2022-11-26

---
+++ BAEKJOON/Platinum/오아시스 재결합.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stack>
+
+#define endl "\n"
+using namespace std;
+
+void Answer()
+{
+    int people_num;
+    cin >> people_num;
+
+    stack<pair<int, int>> st;
+    int now, cnt_same_height = 0;
+    long long  cnt_pair = 0;
+
+    for (int i = 0; i < people_num; ++i) {
+        cin >> now;
+
+        cnt_same_height = 1;
+        while (!st.empty() && st.top().first < now) {
+            cnt_pair += st.top().second;
+            st.pop();
+        }
+
+        if (!st.empty()) {
+            if (st.top().first == now) {
+                cnt_pair += st.top().second;
+                cnt_same_height = (st.top().second + 1);
+                if (st.size() > 1)
+                    ++cnt_pair;
+
+                st.pop();
+            }
+            else {
+                ++cnt_pair;
+            }
+        }
+
+        st.push(make_pair(now, cnt_same_height));
+    }
+
+    cout << cnt_pair << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    Answer();
+}
