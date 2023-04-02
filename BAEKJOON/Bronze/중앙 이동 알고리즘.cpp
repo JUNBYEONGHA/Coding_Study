7472f574e4270b378b4b514b7b2b733f69949319
Jun ByeongHa <qudgk182@gmail.com>
2023-04-02 18:21:38 +0900

    2023-04-02
    2023-04-02

---
+++ BAEKJOON/Bronze/중앙 이동 알고리즘.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+
+#define endl "\n"
+using LL = long long;
+using namespace std;
+
+void Answer()
+{
+	int n;
+	int x = 2;
+	cin >> n;
+	while (n--) {
+		x += (x - 1);
+	}
+	x *= x;
+	cout << x << endl;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	Answer();
+	return 0;
+}
