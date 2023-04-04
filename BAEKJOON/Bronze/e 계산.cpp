9a5fa0bf42a9af01a71d1c57ddfd043f10974aa0
Jun ByeongHa <qudgk182@gmail.com>
2023-04-04 10:41:35 +0900

    2023-04-04
    2023-04-04

---
+++ BAEKJOON/Bronze/e 계산.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+
+#define endl "\n"
+using LL = long long;
+using namespace std;
+
+void Answer()
+{
+	cout << "n e" << endl;
+	cout << "- -----------" << endl;
+	cout << "0 1" << endl;
+	cout << "1 2" << endl;
+	cout << "2 2.5" << endl;
+	cout << "3 2.666666667" << endl;
+	cout << "4 2.708333333" << endl;
+	cout << "5 2.716666667" << endl;
+	cout << "6 2.718055556" << endl;
+	cout << "7 2.718253968" << endl;
+	cout << "8 2.718278770" << endl;
+	cout << "9 2.718281526" << endl;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	Answer();
+	return 0;
+}
