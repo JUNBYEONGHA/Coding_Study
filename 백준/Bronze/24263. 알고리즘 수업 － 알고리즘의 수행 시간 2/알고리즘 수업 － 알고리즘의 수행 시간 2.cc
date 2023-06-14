f77588868ea557afe97980d5cec5a42f9e3a9055
Jun ByeongHa <69623629+JUNBYEONGHA@users.noreply.github.com>
2023-06-14 12:24:13 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <sstream>
+#include <set>
+#include <map>
+#include <climits>
+
+#define endl "\n"
+using LL = long long;
+using namespace std;
+
+void Answer()
+{
+	int n;
+	cin >> n;
+	cout << n << endl << 1;
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	Answer();
+	return 0;
+}
