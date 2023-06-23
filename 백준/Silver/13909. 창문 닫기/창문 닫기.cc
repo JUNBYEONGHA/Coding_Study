1e022deb0dfab3ac33476f47026b1809af1b04f8
Jun ByeongHa <69623629+JUNBYEONGHA@users.noreply.github.com>
2023-06-23 14:16:36 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <sstream>
+#include <set>
+#include <map>
+#include <climits>
+
+#define endl "\n"
+using LL = long long;
+using namespace std;
+
+void Answer()
+{
+	int n;
+	cin >> n;
+	int i = 1;
+	while (i * i <= n) i++;
+	cout << i - 1;
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	Answer();
+	return 0;
+}
