dba0c36f2c89fe9d3cd51618f5ba8441ccb5b829
Jun ByeongHa <69623629+JUNBYEONGHA@users.noreply.github.com>
2023-07-22 00:09:48 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <sstream>
+#include <set>
+#include <map>
+#include <climits>
+
+#define endl "\n"
+using LL = long long;
+using namespace std;
+
+void Answer()
+{
+	cout << "  ___  ___  ___" << endl;
+	cout << "  | |__| |__| |" << endl;
+	cout << "  |           |" << endl;
+	cout << "   \\_________/" << endl;
+	cout << "    \\_______/" << endl;
+	cout << "     |     |" << endl;
+	cout << "     |     |" << endl;
+	cout << "     |     |" << endl;
+	cout << "     |     |" << endl;
+	cout << "     |_____|" << endl;
+	cout << "  __/       \\__" << endl;
+	cout << " /             \\" << endl;
+	cout << "/_______________\\" << endl;
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	Answer();
+	return 0;
+}
