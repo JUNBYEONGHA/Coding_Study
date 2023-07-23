c992aa2e0f7199e309efa5550aa70f63d2c40bfb
Jun ByeongHa <69623629+JUNBYEONGHA@users.noreply.github.com>
2023-07-23 10:18:59 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <sstream>
+#include <set>
+#include <map>
+#include <climits>
+
+#define endl "\n"
+using LL = long long;
+using namespace std;
+
+void Answer()
+{
+	string s; cin >> s;
+	cout << ":fan::fan::fan:" << endl;
+	cout << ":fan::"<< s <<"::fan:" << endl;
+	cout << ":fan::fan::fan:" << endl;
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	Answer();
+	return 0;
+}
