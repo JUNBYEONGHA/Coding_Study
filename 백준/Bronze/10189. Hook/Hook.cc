907f0eafa66ed0021375df0ae04807d7afafb9b4
Jun ByeongHa <69623629+JUNBYEONGHA@users.noreply.github.com>
2023-11-09 09:20:13 +0900

    [Bronze V] Title: Hook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <sstream>
+#include <set>
+#include <map>
+#include <climits>
+
+#define endl "\n"
+using LL = long long;
+using namespace std;
+
+void Answer()
+{
+	cout << "#  # #### #### #  #" << endl
+		<< "#### #  # #  # # #" << endl
+		<< "#### #  # #  # # #" << endl
+		<< "#  # #### #### #  #";
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	Answer();
+	return 0;
+}
